7dd48d43544ae7477c34f7c37d214b5f99b3fa18
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-03 02:31:02 -0700

    Project Images will enlarge when clicked and shrink when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,24 @@
-const hypeRadarImage = document.getElementById("hypeRadarImage");
+// Project Image gets bigger
+const toggleImages = document.querySelectorAll(".toggleImage");
 
-hypeRadarImage.addEventListener("click", function () {
-  hypeRadarImage.style.width = "100%";
-  hypeRadarImage.style.height = "auto";
+toggleImages.forEach((image) => {
+  image.addEventListener("click", function () {
+    let isEnlarged = this.classList.toggle("enlarged-image");
+
+    if (isEnlarged) {
+      this.style.position = "fixed";
+      this.style.top = "50%";
+      this.style.left = "50%";
+      this.style.transform = "translate(-50%, -50%)";
+      this.style.zIndex = "9999";
+      this.style.maxWidth = "60%"; /* Set the maximum width to 100% */
+    } else {
+      this.style.position = "static";
+      this.style.top = "auto";
+      this.style.left = "auto";
+      this.style.transform = "none";
+      this.style.zIndex = "auto";
+      this.style.maxWidth = "25%"; /* Reset the width to the initial value */
+    }
+  });
 });
