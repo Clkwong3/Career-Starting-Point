3e67c567f5a16740bae66e28c6cad22c4089f29a
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-08-03 00:19:28 -0700

    remove extra css. adding js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const hypeRadarImage = document.getElementById("hypeRadarImage");
+
+hypeRadarImage.addEventListener("click", function () {
+  hypeRadarImage.style.width = "100%";
+  hypeRadarImage.style.height = "auto";
+});
